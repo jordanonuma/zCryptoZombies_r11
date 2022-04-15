c16bf12294b40357d49f502cfdc9f4d19bc2889f
jordanonuma <jordanonuma@outlook.com>
2022-04-15 09:06:11 -1000

    Takes the modulus so DNA integer will have 16 digits and uses the average of the DNA integers to create new zombie.

--- ZombieFeeding.sol
+++ ZombieFeeding.sol
@@ -6,6 +6,10 @@ contract ZombieFeeding is ZombieFactory {
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
         require(msg.sender == zombieToOwner[_zombieId]);
         Zombie storage myZombie = zombies[_zombieId];
+
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        createZombie("NoName", newDna);
     } //end function feedAndMultiply()
 
 } //end contract ZombieFeeding{}
