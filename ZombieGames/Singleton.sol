ac709ae126c3cfed1093f66db74ed60a5376033b
Jordan <jordanonuma@outlook.com>
2022-06-18 08:11:54 -1000

    Revised syntax.

--- ZombieGames/Singleton.sol
+++ ZombieGames/Singleton.sol
@@ -21,7 +21,7 @@ contract Singleton is ZBGameMode  {
                 bool cardAlreadyInDeck = false;
 
                 for (uint k = 0; k < cardCount; k++) {
-                    if (keccak256(abi.encodePacked(newCards[k].mouldName)) == gameState.playerStates[i].cardsInDeck[j].mouldName]) {
+                    if (keccak256(abi.encodePacked(newCards[k].mouldName)) == keccak256(abi.encodePacked(gameState.playerStates[i].cardsInDeck[j].mouldName))) {
                         cardAlreadyInDeck = true;
                     } //end if()
                 } //end for()
