4af0721b34e5f4169b349362e04e5f98f6ee66a6
Jordan <jordanonuma@outlook.com>
2022-06-23 08:45:26 -1000

    Loads artifacts of the contract to interact.

---
+++ Testing/CryptoZombies.js
@@ -0,0 +1,6 @@
+const CryptoZombies = artifacts.require(“CryptoZombies");
+contract("CryptoZombies", (accounts) => {
+    it("should be able to create a new zombie", () => {
+
+    })
+}) //end contract()
