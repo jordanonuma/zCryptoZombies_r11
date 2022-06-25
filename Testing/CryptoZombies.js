0da29ddbb2eb999165c0364cd13259bede2b7cb2
Jordan <jordanonuma@outlook.com>
2022-06-25 10:33:59 -1000

    Instantiated contract for testing.

--- Testing/CryptoZombies.js
+++ Testing/CryptoZombies.js
@@ -2,6 +2,6 @@ const CryptoZombies = artifacts.require(“CryptoZombies");
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
     it("should be able to create a new zombie", async () => {
-
+        const contractInstance = await CryptoZombies.new();
     }) //end it()
 }) //end contract()
