cfd9da5cf5d8bc1222e143b430e46574700d232c
Jordan <jordanonuma@outlook.com>
2022-06-27 07:17:42 -1000

    Added the hook that creates a new contract instance and checks for zombie duplicates.

--- Testing/CryptoZombies.js
+++ Testing/CryptoZombies.js
@@ -1,11 +1,20 @@
 const CryptoZombies = artifacts.require(“CryptoZombies");
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
+    let contractInstance;
+
     it("should be able to create a new zombie", async () => {
         const contractInstance = await CryptoZombies.new();
+        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
 
-        const result = contractInstance.createRandomZombie(zombieNames[0], {from: alice});
         assert.equal(result.receipt.status, true);
         assert.equal(result.logs[0].args.name, zombieNames[0]);
     }) //end it()
+
+    beforeEach(async () => {
+        contractInstance = await CryptoZombies.new();
+        it("should not allow two zombies", async () => {
+            
+        }) //end it()
+    }); // end beforeEach()
 }) //end contract()
