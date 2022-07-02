07f2258984c7083990721e248a2a46e52fd97acf
Jordan <jordanonuma@outlook.com>
2022-07-02 10:19:50 -1000

    Similar but Alice calls the transfer to Bob.

--- Testing/CryptoZombies.js
+++ Testing/CryptoZombies.js
@@ -44,7 +44,12 @@ contract("CryptoZombies", (accounts) => {
         }) //end it()
 
         it("should approve and then transfer a zombie when the owner calls transferFrom", async () => {
-            // TODO: Test the two-step scenario.  The owner calls transferFrom
+            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            await contractInstance.approve(bob, zombieId, {from: alice});
+            await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
+            const newOwner = await contractInstance.ownerOf(zombieId);
+            assert.equal(newOwner,bob);
         })
     }) //end context()
 
