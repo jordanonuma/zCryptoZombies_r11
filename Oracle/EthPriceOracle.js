8af07382790c0c7dc175cae8a085742aa54c4161
Jordan <jordanonuma@outlook.com>
2022-07-25 11:01:50 -1000

    Redefined the multiplier parameters.

--- Oracle/EthPriceOracle.js
+++ Oracle/EthPriceOracle.js
@@ -66,8 +66,8 @@ async function processRequest(oracleContract, ownerAddress, id, callerAddress) {
 } //end function processRequest()
 
 async function setLatestEthPrice (oracleContract, callerAddress, ownerAddress, ethPrice, id) {
-    ethPrice = ethPrice.replace('.'.'')
-    const multiplier = new BN(multiplier,10)
+    ethPrice = ethPrice.replace('.','')
+    const multiplier = new BN(10**10,10)
     
     const ethPriceInt = (new BN(parseInt(ethPrice), 10)).mul(multiplier)
     const idInt = new BN(parseInt(id))
