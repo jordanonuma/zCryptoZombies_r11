0ae499a7f141e129532637db5167a95f7d174d66
Jordan <jordanonuma@outlook.com>
2022-08-09 11:32:56 -1000

    Establishes Ethereum provider and signers for transfer between chains and on zkSync.

--- Oracle/Utils/Utils.js
+++ Oracle/Utils/Utils.js
@@ -8,3 +8,20 @@ async function getZkSyncProvider(zksync, networkName) {
     }
     return zkSyncProvider
 } //end function getZkSyncProvider()
+
+async function getEthereumProvider (ethers, networkName) {
+    let ethersProvider
+    try {
+      // eslint-disable-next-line new-cap
+      ethersProvider = new ethers.getDefaultProvider(networkName)
+    } catch (error) {
+      console.log('Could not connect to Rinkeby')
+      console.log(error)
+    }
+    return ethersProvider
+} //end getEthereumProvider()
+
+async function initAccount (rinkebyWallet, zkSyncProvider, zkSync) {
+    const zkSyncWallet = await zksync.Wallet.fromEthSigner(rinkebyWallet, zkSync)
+    return zkSyncWallet
+} //end initAccount()
